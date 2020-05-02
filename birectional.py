79dcec4d4c293fd4a74d9f8419708920198fdab5
Ccchloew <47759817+Ccchloew@users.noreply.github.com>
2020-05-02 13:22:03 -0400

    Update birectional.py

--- birectional.py
+++ birectional.py
@@ -14,25 +14,27 @@ stop = time.time() +  120.0
 #data = ['\n','u','1','2','\r']
 
 
-#while time.time() < stop:
+while time.time() < stop:
 #x = pi.xfer([1]) #send integers/long using an array
 #tx = pi.writebytes([ord('h')]) #send a single character
-#x = pi.readbytes(1)
-ports = ["ULTRASONIC","3"]
-portname = ports[0]
-portnumber = str(ports[1])
-portsdict = {
-	"LMOTOR" : "LM",
-        "RMOTOR" : "RM",
-        "MOTOR3" : "M",
-        "LINE" : "L",
-        "INFARED" : "I",
-        "RFID" : "R",
-        "ULTRASONIC" : "U"
-    }
-arr = ['\n'] + list(portnumber) + list(portsdict[portname]) + ['\r']
-for i in range(len(arr)):
-	tx = pi.writebytes([ord(arr[i])]) #send a single character
-	print(i)
+	x = pi.readbytes(1)
+	print(x)
+	
+# ports = ["ULTRASONIC","3"]
+# portname = ports[0]
+# portnumber = str(ports[1])
+# portsdict = {
+# 	"LMOTOR" : "LM",
+#         "RMOTOR" : "RM",
+#         "MOTOR3" : "M",
+#         "LINE" : "L",
+#         "INFARED" : "I",
+#         "RFID" : "R",
+#         "ULTRASONIC" : "U"
+#     }
+# arr = ['\n'] + list(portnumber) + list(portsdict[portname]) + ['\r']
+# for i in range(len(arr)):
+# 	tx = pi.writebytes([ord(arr[i])]) #send a single character
+# 	print(i)
 
 pi.close()
