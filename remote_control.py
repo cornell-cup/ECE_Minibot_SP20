0dd3963e001c6a2744ce396d09fcbe7d6c584e3f
yanruyu <32486980+yanruyu126@users.noreply.github.com>
2020-05-22 10:47:38 -0400

    Create remote_control.py

---
+++ remote_control.py
@@ -0,0 +1,6 @@
+# It's only a reserved place for the code... Unfortunately I don't have it right now since I couldn't get access
+# to the Pi where I stored all my files for the Baby Yoda software project.
+
+# Contact Ruyu Yan if you need the code now and I could explain it to you.
+
+# Hopefully it will get updated by June.
