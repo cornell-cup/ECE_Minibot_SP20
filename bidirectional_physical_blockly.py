a4991281ad3a2a717fa0a5c3572f804f3fc72bce
Ccchloew <47759817+Ccchloew@users.noreply.github.com>
2020-10-10 15:03:52 -0400

    Update bidirectional_physical_blockly.py

--- bidirectional_physical_blockly.py
+++ bidirectional_physical_blockly.py
@@ -9,30 +9,30 @@ pi.max_speed_hz = 115200
 stop = time.time() +120.0
 
 while (time.time() < stop):
- 
 	x = pi.readbytes(1)
-	print(x)
-
-if x == 0:
-    data = ['\n','s']
-
-elif x == 1:
-    data = ['\n','t','l']
-
-elif x == 1:
-    data = ['\n','t','r']
-elif x == 1:
-    data = ['\n','t','a']
-else:
-    data = []
-
-pi.close()
-
-a = pi.open(0,0)
-pi.mode = 0
-pi.max_speed_hz = 9600
-
-for i in range(len(data)):
+	pi.close()
+	a = pi.open(0,0)
+	pi.mode = 0
+	pi.max_speed_hz = 115200
+	if x == [0]:
+		data = ['\n','s']
+	elif x == [1]:
+		data = ['\n','t','l']
+	elif x == [2]:
+		data = ['\n','t','r']
+	elif x == [3]:
+		data = ['\n','t','a']
+	else:
+		data = []
+	for i in range(len(data)):
  	tx = pi.writebytes([ord(data[i])]) #send a single character
 
-pi.close()
+	pi.close()
+	h = pi.open(0,1)
+	pi.mode = 0
+	pi.max_speed_hz = 115200
+
+
+
+
+
