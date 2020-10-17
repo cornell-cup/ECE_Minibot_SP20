46601e35e51a88133152259ba4e1946b1be5ed29
Jiayao Wu <jw995@cornell.edu>
2020-10-17 15:24:18 -0400

    changes

--- bidirectional_physical_blockly.py
+++ bidirectional_physical_blockly.py
@@ -8,7 +8,7 @@ pi = spidev.SpiDev()
 #spi.open(bus, device)
 #cs1 is RFID
 #cs0 is motor
-pi.open(0,cs1)
+pi.open(0,1)
 pi.mode = 0
 pi.max_speed_hz = 115200
 stop = time.time() +120.0
@@ -16,16 +16,16 @@ stop = time.time() +120.0
 while (time.time() < stop):
 #if there is input, read from device 1
 	x = pi.readbytes(1)
+	if x != [0]:
+	    print(x)
 	pi.close()
 	pi.open(0,cs0)
 	pi.mode = 0
 	pi.max_speed_hz = 115200
-	if x == [0]:
-		data = ['\n','s']
-	elif x == [1]:
-		data = ['\n','t','l']
+	if x == [1]:
+		data = ['\n','c','c']
 	elif x == [2]:
-		data = ['\n','t','r']
+		data = ['\n','c','w']
 	elif x == [3]:
 		data = ['\n','t','a']
 	else:
@@ -38,6 +38,7 @@ while (time.time() < stop):
 	pi.mode = 0
 	pi.max_speed_hz = 115200
 
+pi.close()
 
 
 
